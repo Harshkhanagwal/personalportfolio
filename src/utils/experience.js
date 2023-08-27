b1ecedaca33ae507250c3df58d6cd7d8dd1b8857
Harsh khanagwal <91535787+Harshkhanagwal@users.noreply.github.com>
2023-08-27 15:44:28 +0530

    Update experience.js
    Adding my internship experience

--- src/utils/experience.js
+++ src/utils/experience.js
@@ -1,5 +1,12 @@
 const experience = [
     {
+        company_name : "Radionics Technology",
+        post : "Web Development Intern",
+        date_started : "July 2023",
+        date_end : "Aug 2023",
+        comapany_logo : "https://media.licdn.com/dms/image/C4D0BAQGBFWfvWI_bjg/company-logo_200_200/0/1653861124068?e=1701302400&v=beta&t=INj4QoaG92nahEGr0h0D1onX8gUylDSmrOTn7FcQoVM"
+    },
+    {
         company_name : "Freelance Developer",
         post : "Frontend Web Development, Specializing in React JS",
         date_started : "July 2021",
